f40a73c1fec7a174447a8d972b2a51896a98a7c6
David Young <david@textflex.com>
2015-09-15 01:21:43 +0000

    refactored to use build-setup.sh

--- build.sh
+++ build.sh
@@ -85,50 +85,16 @@ CLEAN=0
 PAR_JAR="--jar"
 JAR=0
 
-echo -n "Detecting environment..."
-SYSTEM=`uname -s`
-CYGWIN="false"
-LINUX="false"
-MAC="false"
-if [ `expr "$SYSTEM" : "CYGWIN"` -eq 6 ]
+# Sets the base directory to the script location
+if [ "x$BASE_DIR" = "x" ] # empty string
 then
-	CYGWIN="true"
-elif [ `expr "$SYSTEM" : "Linux"` -eq 5 ]
-then
-	LINUX="true"
-
-	# Java binary detection mechanism
-	if [ "`command -v /usr/lib/jvm/java-1.6.0/bin/javac`" != "" ]
-	then
-		# OpenJDK directory on Fedora distributions
-		JAVA="/usr/lib/jvm/java-1.6.0/bin"
-	elif [ "`command -v /usr/lib/jvm/java-6-openjdk/bin/javac`" != "" ]
-	then
-		# OpenJDK directory on Ubuntu distributions
-		JAVA="/usr/lib/jvm/java-6-openjdk/bin"
-	elif [ "`command -v /usr/java/default/bin/javac`" != "" ]
-	then
-		JAVA="/usr/java/default/bin"
-	elif [ "`command -v javac`" != '' ]
-	then
-		JAVA=""
-	elif [ "`command -v /usr/bin/javac`" != "" ]
-	then
-		JAVA="/usr/bin"
-	else
-		JAVA="false"
-		echo "Java software doesn't appear to be installed..."
-		echo "Please download it (for free!) from http://java.com."
-		echo "Or if it's already installed, please add it to your"
-		echo "PATH or to the JAVA variable in this script."
-		read -p "Press Enter to exit this script..."
-		exit 1
-	fi
-elif [ `expr "$SYSTEM" : "Darwin"` -eq 6 ]
-then
-	MAC="true"
+	BASE_DIR=`dirname $0`
 fi
-echo "found $SYSTEM"
+cd "$BASE_DIR"
+BASE_DIR="$PWD"
+
+# Platform and GUI detection
+source "$BASE_DIR"/../texttrix/trunk/build-setup.sh
 
 ##############
 # Respond to user arguments
@@ -190,14 +156,6 @@ then
 	# present to prevent double separators
 	JAVA=${JAVA%\/}/
 fi
-
-# Sets the base directory to the script location
-if [ "x$BASE_DIR" = "x" ] # empty string
-then
-	BASE_DIR=`dirname $0`
-fi
-cd "$BASE_DIR"
-BASE_DIR="$PWD"
 
 #####################
 # Build operations
